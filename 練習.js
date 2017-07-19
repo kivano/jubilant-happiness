0a87dcf46b9f2facd75fddb5300f8562f452d918
kivano <rigel.zeno@gmail.com>
2017-07-19 13:17:29 +0900

    Update 練習.js

--- 練習.js
+++ 練習.js
@@ -1,6 +1,14 @@
 $(function(){
+	var english =["hair dryer","blinker","plastic bottle","laptop","gas station","flip flop"];
+	english[0] = "hair dryer";
+	english[1] = "blinker";
+	english[2] = "plastic bottle";
+	english[3] = "laptop";
+	english[4] = "gas station";
+	english[5] = "flip flop";
+
 	$("#getbutton").click(function(){
 	//document.writeln("Hair Dryer");
-          $("#hairdryer").html("Hair Dryer")
+          $("#hairdryer").html(english[0])
           });
 });
