9212f1ad589bcd6137a4d179b8714338012c3724
kivano <rigel.zeno@gmail.com>
2017-09-10 12:34:04 +0900

    Create Bus Booking.js

---
+++ Bus Booking.js
@@ -0,0 +1,4 @@
+ //バス予約用ホームぺージ 
+2 //Bus Booking System 
+3 //私たちは直接コミュニケーションをとることが難しいので、なるべくコードにコメントをつけるようにしましょう! 
+4 //Write cords with comments as possible as we can, because we don't have enough time to communicate directly. 
